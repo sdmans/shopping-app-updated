c9aaec55319dd2928b6da049b76c275e6f95e04b
“Sam <smans123@gmail.com>
2019-07-24 07:58:24 -0400

    Created test for retrieving values from database, currently testing observable filter method

--- shopping-app/src/app/app.component.ts
+++ shopping-app/src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+/* map is an operator that transforms data by applying a function. More about Map here https://blog.angularindepth.com/reading-the-rxjs-6-sources-map-and-pipe-94d51fec71c2 */  
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
+/* Of emits any number of provided values in sequence. More about the 'of' operator here: https://www.learnrxjs.io/operators/creation/of.html */
+import { of } from 'rxjs';
+
+export interface Comment { comment: string, name: string, userId: string, id: string}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +16,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shopping-app';
+  $comments: Observable<Comment[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.$comments = this.afs.collection<Comment>('comments').valueChanges();
+  }
+
+  ngOnInit() {
+    this.$comments.subscribe(data => console.log(data));
+  }
 }
