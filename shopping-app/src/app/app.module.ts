aed5e3532a146fe030b277b03d4125d0eb62dd8d
“Sam <smans123@gmail.com>
2019-07-24 07:02:23 -0400

    Adding firebase to app module and environment, excluding environment from commit for test

--- shopping-app/src/app/app.module.ts
+++ shopping-app/src/app/app.module.ts
@@ -4,6 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Importing modules for Angular Fire and Firebase */
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { environment } from '../environments/environment';
+
+
 /* Importing Angular Material Modules */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
@@ -14,6 +21,9 @@ import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/mat
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule, // imports firebase/firestore, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
     BrowserAnimationsModule,
     AppRoutingModule,
     MatButtonModule, 
--- shopping-app/src/environments/environment.ts
+++ shopping-app/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCpAqzZCXig9a7_WTkxIhIo4L1ovBgUuYA",
+    authDomain: "tutorialdb-acd1a.firebaseapp.com",
+    databaseURL: "https://tutorialdb-acd1a.firebaseio.com",
+    projectId: "tutorialdb-acd1a",
+    storageBucket: "tutorialdb-acd1a.appspot.com",
+    messagingSenderId: "736234426774"
+  }
 };
 
 /*
