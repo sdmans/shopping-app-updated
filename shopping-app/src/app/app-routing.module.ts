5ac047cd9db6c83fd8a1fd6fb7cd256e43846192
“Sam <smans123@gmail.com>
2019-07-25 07:59:04 -0400

    Adding routes for components and testing to make sure they work

--- shopping-app/src/app/app-routing.module.ts
+++ shopping-app/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+/* Importing components */
+import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AccountComponent } from './components/account/account.component';
+import { LoginComponent } from './components/login/login.component';
+import { SubmitComponent } from './components/submit/submit.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'submit', component: LoginComponent },
+  { path: 'account', component: AccountComponent },
+  { path: 'profile', component: LoginComponent },
+  { path: '',
+  redirectTo: 'main',
+  pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shopping-app/src/app/app.module.ts
+++ shopping-app/src/app/app.module.ts
@@ -2,7 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+/* Imported components */
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AccountComponent } from './components/account/account.component';
+import { LoginComponent } from './components/login/login.component';
+import { SubmitComponent } from './components/submit/submit.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 /* Importing modules for Angular Fire and Firebase */
 import { AngularFireModule } from '@angular/fire';
@@ -14,12 +21,6 @@ import { environment } from '../environments/environment';
 /* Importing Angular Material Modules */
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
-import { MainComponent } from './components/main/main.component';
-import { RegisterComponent } from './components/register/register.component';
-import { AccountComponent } from './components/account/account.component';
-import { LoginComponent } from './components/login/login.component';
-import { SubmitComponent } from './components/submit/submit.component';
-import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
