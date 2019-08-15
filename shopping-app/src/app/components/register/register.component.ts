74c99b53e98fe1a7b61370d78ed6c7780ed9df9e
“Sam <smans123@gmail.com>
2019-08-15 07:53:53 -0400

    Cleaning up markup and testing out registration form inputs with password match check

--- shopping-app/src/app/components/register/register.component.ts
+++ shopping-app/src/app/components/register/register.component.ts
@@ -12,4 +12,20 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
   }
 
+  register(userName, email, password, verifyPassword): void {
+    // console.log(userName.value, email.value, password.value, verifyPassword.value);
+
+    if (password.value === verifyPassword.value) {
+      const user = {
+        name: userName.value,
+        email: email.value,
+        password: password.value
+      }
+      console.log(user);
+    } else {
+      console.log("Passwords don't seem to match, please re-enter your credentials");
+    }
+      
+  }
+
 }
